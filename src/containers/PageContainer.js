183688277abdbd1e340629db181b06b6f1175f23
Ad Infinitum <35665239+poee@users.noreply.github.com>
2018-11-02 12:19:57 -0400

    Refactor

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -1,54 +1,61 @@
-import React from 'react'
-import { compose, withProps, withStateHandlers } from 'recompose'
+import React, { PureComponent } from 'react'
 import Routes from 'react-static-routes'
 import Header from '../comps/Header'
 import Menu from './Menu'
 
-function PageContainer(props) {
-	return (
-		<div className="pageContainer" style={props.sizeStyle} key="container">
-			<Menu />
-			<article className="content">
-				<Header {...props} />
-				<Routes />
-			</article>
-		</div>
-	)
-}
 
-const INCREMENT = 10
 const BASE_FONT = 95
+const INCREMENT = 10
 export const MIN_FONT = 75
 export const MAX_FONT = 155
-function getLSValue() {
-	return typeof window !== 'undefined' && Number(window.localStorage.getItem('fontSize')) || BASE_FONT
-}
-function updateSize(oldValue, change) {
-	oldValue = oldValue || getLSValue()
-	let fontSize = Math.max(MIN_FONT, oldValue + change)
-	fontSize = Math.min(MAX_FONT, fontSize)
-	return {
-		fontSize,
-	}
-}
 
-export default compose(
-	withStateHandlers(
-		{ fontSize: null }, {
-			decrease: ({ fontSize }) => () => updateSize(fontSize, -INCREMENT),
-			increase: ({ fontSize }) => () => updateSize(fontSize, +INCREMENT),
-		}
-	),
-	withProps(({ fontSize }) => {
-		if (typeof window !== 'undefined') {
-			fontSize = fontSize || getLSValue()
-			window.localStorage.setItem('fontSize', fontSize)
-		}
-		return {
+class PageContainer extends PureComponent {
+	constructor(props) {
+		super(props)
+		this.inBrowser = typeof window !== 'undefined'
+		this.state = { fontSize: null }
+	}
+
+	componentDidMount() {
+		// Check through localStorage once
+		this.update(0)
+	}
+
+	increase = () => {
+		this.update(INCREMENT)
+	}
+	decrease = () => {
+		this.update(-INCREMENT)
+	}
+	update = (change) => {
+		let fontSize = this.state.fontSize || (
+			this.inBrowser && Number(window.localStorage.getItem('fontSize'))
+		) || BASE_FONT
+		fontSize = Math.max(MIN_FONT,
+			Math.min(MAX_FONT, fontSize + change)
+		)
+
+		this.inBrowser && window.localStorage.setItem('fontSize', fontSize)
+		this.setState({
 			fontSize,
 			sizeStyle: {
 				fontSize: `${fontSize / 100}rem`
 			}
-		}
-	})
-)(PageContainer)
+		})
+	}
+
+	render() {
+		return (
+			<div className="pageContainer" style={this.state.sizeStyle} key="container">
+				<Menu />
+				<article className="content">
+					<Header {...this.state} decrease={this.decrease} increase={this.increase} />
+					<Routes />
+				</article>
+			</div>
+		)
+	}
+}
+
+
+export default PageContainer
